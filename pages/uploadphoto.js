22ccc099268cd3c0a86f37c943d958fb93e779b8
Muneeb Baig <109512760+muneeb-hub-23@users.noreply.github.com>
2024-06-15 11:37:07 +0500

    dev converted into build
    build command executed yarn build or npm run build

--- pages/uploadphoto.js
+++ pages/uploadphoto.js
@@ -1,22 +1,31 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
+
+// const uploadphoto = () => {
+// const [myfile,setMyFile]=useState('')
+// const handleSubmit = (e)=>{
+//     e.preventDefault()
+//     console.log(myfile)
+// }
+// const handleChange = (e)=>{
+//     setMyFile(e.target.value)
+// }
+//   return (
+//     <div>
+//         <form onSubmit={handleSubmit}>
+//         <label htmlFor="myfile">Select a file:</label>
+//         <input type="file" id="myfile" value={myfile} onChange={handleChange} name="myfile" />
+//         <button type='submit'>Submit</button>
+//         </form>
+//     </div>
+//   )
+// }
+
+// export default uploadphoto
+import React from 'react'
 
 const uploadphoto = () => {
-const [myfile,setMyFile]=useState('')
-const handleSubmit = (e)=>{
-    e.preventDefault()
-    console.log(myfile)
-}
-const handleChange = (e)=>{
-    setMyFile(e.target.value)
-}
   return (
-    <div>
-        <form onSubmit={handleSubmit}>
-        <label htmlFor="myfile">Select a file:</label>
-        <input type="file" id="myfile" value={myfile} onChange={handleChange} name="myfile" />
-        <button type='submit'>Submit</button>
-        </form>
-    </div>
+    <div>uploadphoto</div>
   )
 }
 
